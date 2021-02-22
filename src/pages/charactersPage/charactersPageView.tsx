f2de9cda4088111117546ed3e4fe1b4e7682cbc4
Акулович Виталий Леонидович <vitali.akulovich@mrsoft.by>
2021-02-22 20:58:49 +0300

    feat: add virtual scrolling

--- src/pages/charactersPage/charactersPageView.tsx
+++ src/pages/charactersPage/charactersPageView.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 import { LoadingComponent } from 'components';
 import { ICharacterInfo } from 'shared/interfaces/store';
@@ -18,29 +19,33 @@ export const CharactersPageView: React.FC<ICharactersPageProps> = ({
     getInitData,
     getNextCharactersPage,
 }: ICharactersPageProps) => {
-    useEffect(() => {
-        getInitData();
-    }, []);
+    const [lastRowRef, lastRowInView] = useInView();
 
-    const onNextPage = () => {
-        if (nextPageUrl) {
-            getNextCharactersPage(nextPageUrl);
+    useEffect(() => {
+        if (charactersList?.length) {
+            if (nextPageUrl && lastRowInView) {
+                getNextCharactersPage(nextPageUrl);
+            }
+        } else {
+            getInitData();
         }
-    };
+    }, [lastRowInView]);
 
     if (isInitLoading) {
         return <LoadingComponent />;
     }
     return (
         <div>
-            {charactersList?.map((character) => (
-                <div key={character.id}>
-                    <img width={50} height={50} src={character.avatarSrc} alt="Avatar" />
-                    <b>{character.name}</b>
-                    <hr />
-                </div>
-            )) || 'Oops, no characters :('}
-            {nextPageUrl && <button onClick={onNextPage}>More characters</button>}
+            {charactersList?.map((character, index) => {
+                const ref = index === charactersList.length - 1 ? lastRowRef : null;
+                return (
+                    <div key={character.id} ref={ref}>
+                        <img width={50} height={50} src={character.avatarSrc} alt="Avatar" />
+                        <b>{character.name}</b>
+                        <hr />
+                    </div>
+                );
+            }) || 'Oops, no characters :('}
         </div>
     );
 };
